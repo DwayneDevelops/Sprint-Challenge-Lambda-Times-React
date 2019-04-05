7c2a6bff74d140e2b5456396fe2ca87663387a3c
DwayneDevelops <mrsuccessfuldev@gmail.com>
2019-04-05 10:55:37 -0500

    cards showing although not populated

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+   return (
+      <div className="cards-container">
+        {this.props.cards.map((c, i )=> <Card card={c} key={i}/> )}
+      </div>
+    ) 
+  }
+  
 }
 
 // Make sure you include prop types for all of your incoming props
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map( (t, i) => <Tab tab={t} key={i} /> )}
       </div>
     </div>
   );
